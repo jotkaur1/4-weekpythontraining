5c6b23d57209439fb44bc8fb994a93c9f9c55bbe
Manjot kaur <109578923+jotkaur1@users.noreply.github.com>
2022-08-08 11:55:37 +0530

    Add files via upload

---
+++ 14.7.22(operators).py
@@ -0,0 +1,31 @@
+a=10
+b=5
+print("arithmetic operations:")
+print("sum=",a+b)
+
+print("difference=",a-b)
+print("product=",a*b)
+print("division=",a/b)
+print("lower division=",a//b)
+print("exponent=",a**b)
+print("comparison operators:")
+print(a>b)
+print(a<b)
+print(a==b)
+print(a>=b)
+print(a!=b)
+print(not(a<b))
+print("logical operators:")
+print("and:",15>4 and 15>20)
+print("or:",15>4 or 15>20)
+print("assignment operators:")
+x=100
+print("x:",x)
+x+=2
+print("x+=2:",x)
+m=1010001
+n=1000101
+print(m&n)
+print(m|n)
+print(m^n)
+print(~a)
